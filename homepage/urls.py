ffb3ccad48b267eb28ef93c62499f320b82c21cf
Nuk0lun <Demjannyk@gmail.com>
2025-08-19 22:05:50 +0300

    second update
    Signed-off-by: Nuk0lun <Demjannyk@gmail.com>

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
-from .views import homeview
+from .views import homeview, aboutview, infoview
 
 urlpatterns = [
-    path('',homeview,name='homepage')
+    path('',homeview,name='homepage'),
+    path('about/',aboutview,name='aboutpage'),
+    path('info/',infoview,name='infopage'),
 ]
-# 192.168.137.1
-# 192.168.0.1
-# 
--- homepage/views.py
+++ homepage/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def homeview(request):
     return render(request, 'index.html')
+
+def aboutview(request):
+    return render(request, 'about.html')
+
+def infoview(request):
+    return render(request, 'info.html')
